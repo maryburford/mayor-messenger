eae03afb859a1f1493cfb2a1472b6d5d21479396
Mary Burford <burfordm@gmail.com>
2026-01-18 15:31:05 -0500

    bigger

--- src/App.tsx
+++ src/App.tsx
@@ -170,7 +170,7 @@ return (
   <img
     src="/fat-tortie.png"
     alt="Fat tortie"
-    className="w-40 h-auto"
+    className="w-50 h-auto"
   />
     <h2 className="text-lg font-bold">Message your Jersey City Councilperson</h2>
 
